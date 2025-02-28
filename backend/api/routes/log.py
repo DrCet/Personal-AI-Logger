1e3fde86bb317f987e3ef5472af91a9fdd054570
DrCet <cetdocteurhehe@gmail.com>
2025-03-01 00:43:13 +0700

    Update project structure and API endpoints

---
+++ backend/api/routes/log.py
@@ -0,0 +1,22 @@
+#backnend/api/routes/log.py
+#this module defines the API endpoints for the logs table
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from backend.models import Log
+from backend.database import get_db
+
+
+router = APIRouter()
+
+class LogRequest(BaseModel):
+    text: str
+
+@router.post('/log', summary='Create a new log entry')
+def create_log(log_request: LogRequest, db: Session = Depends(get_db)):
+    # Create a new log instance with the provided text
+    new_log = Log(text=log_request.text)
+    db.add(new_log) 
+    db.commit()
+    db.refresh(new_log)
+    return {'message': "Log saved", 'id':new_log.id}
---
+++ backend/models.py
@@ -0,0 +1,18 @@
+# backend/models.py
+# This file defines the database models for the Personal AI Logger project using SQLAlchemy.
+# Models represent the structure of the database tables and how data is stored and retrieved.
+
+from sqlalchemy import Column, Integer, String, DateTime
+import datetime
+from backend.database import Base
+
+class Log(Base):
+    # Defie the name of table in the database
+    __tablename__ = "logs"
+    # 'id' is the primary key for the logs table
+    id = Column(Integer, primary_key=True, index=True)
+    # 'text' is a column in the logs table that stores the text of the log
+    text = Column(String)
+    # timestamp is a column in the logs table that stores the timestamp
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
